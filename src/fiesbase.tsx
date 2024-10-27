c48b8804cc225726a0252d3ccc18bf04e3212849
taehee.rho <taeheerho@gmail.com>
2024-10-27 20:41:33 +0900

    init project

---
+++ src/fiesbase.tsx
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDGjNf4UhQyvlYKhagLceNyY_HqMGFCwGo",
+  authDomain: "nwitter-reloaded-1aae9.firebaseapp.com",
+  projectId: "nwitter-reloaded-1aae9",
+  storageBucket: "nwitter-reloaded-1aae9.appspot.com",
+  messagingSenderId: "382683740570",
+  appId: "1:382683740570:web:ac9073cb3f58f61379a223",
+};
+
+const app = initializeApp(firebaseConfig);
